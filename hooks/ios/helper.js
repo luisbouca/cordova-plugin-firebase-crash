0a625b2d4995aec5cbd7838824400f912f4b4b78
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-03-03 13:59:56 +0000

    fix: use require instead of requireCordovaModule for xcode

--- hooks/ios/helper.js
+++ hooks/ios/helper.js
@@ -4,7 +4,7 @@ module.exports = {
     BUILD_PHASE_COMMENT: "Crashlytics",
 
     getXcodeProjectPath: function(context) {
-        const ConfigParser = context.requireCordovaModule("cordova-lib").configparser;
+        const ConfigParser = context.require("cordova-lib").configparser;
         const appName = new ConfigParser("config.xml").name();
         return path.join("platforms", "ios", appName + ".xcodeproj", "project.pbxproj");
     }
